4a6d997543312a6e3ee85e56d50578dd938bfd09
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-09-05 09:17:48 -0300

    Projeto inicial do módulo 14.

--- Persistence-Pro/Persistence-Pro/ContentView.swift
+++ Persistence-Pro/Persistence-Pro/ContentView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
+
+class Coordinator: NSObject, ARSessionDelegate {
+    
+    var arView: ARView?
+
+    @objc func onTap(_ recognizer: UITapGestureRecognizer) {
+        
+        guard let arView = arView else {
+            return
+        }
+
+        let location = recognizer.location(in: arView)
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let result = results.first {
+            
+            let anchor = AnchorEntity(raycastResult: result)
+            let box = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [SimpleMaterial(color: .green, isMetallic: true)])
+            anchor.addChild(box)
+            
+            arView.scene.addAnchor(anchor)
+        }
+    }
+}
 
 struct ContentView : View {
+    
     var body: some View {
+        
         return ARViewContainer().edgesIgnoringSafeArea(.all)
+        
     }
 }
 
@@ -19,18 +47,18 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.onTap)))
+        context.coordinator.arView = arView
+        arView.session.delegate = context.coordinator
         
         return arView
-        
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
     
 }
 
